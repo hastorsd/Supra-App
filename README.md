340f2d67759ff10ac574e7ec1f2282116a8598f5
Hasto <hastorostamhadi@gmail.com>
2023-09-14 20:19:41 +0700

    Tugas Navigation

--- README.md
+++
@@ -1,7 +0,0 @@
-# Supra-App
-Project Pengembangan Aplikasi Mobile Supra App
-
-Kelas J.2
-Anggota :
-1. Hasto Rostamhadi 21537144023
-2. Muhammad Naufal Alfawwaz 21537144024
---
+++ app/src/main/java/com/example/supraapp/ui/WelcomePage.kt
@@ -0,0 +1,23 @@
+package com.example.supraapp.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.supraapp.R
+import com.example.supraapp.databinding.ActivityWelcomePageBinding
+
+class WelcomePage : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWelcomePageBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityWelcomePageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonToHome.setOnClickListener {
+            startActivity(Intent(this, Home::class.java))
+            finish()
+        }
+    }
+}
